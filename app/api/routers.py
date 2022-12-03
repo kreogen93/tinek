c6591d07d588942246b5c83d4f5667d79d1f3582
kyoto <shterns.live@mail.ru>
2022-12-03 17:00:17 +0300

    Упростил ручки

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,47 +1,23 @@
 from fastapi import APIRouter
 
-from app.schemas.schema import Match, Score, Stage, Team, Tournament
+
+from app.services import tournament
 
 router = APIRouter(
     prefix="/api",
 )
 
 
-
 @router.get("/tournaments/{t_id}")
 async def get_tournament(t_id: str):
-    return {"t_id": t_id}
-
-
-
+    return tournament.get_tournament(t_id)
 
 
 @router.post("/tournaments")
 def generate_tournament():
-    return Tournament(
-        id=1,
-        name="name",
-        status="ACTIVE",
-        stages=[
-            Stage(
-                level=1,
-                matches=[
-                    Match(
-                        id=1,
-                        team1=Team(id=1, name="name1"),
-                        team2=Team(id=2, name="name2"),
-                        score1=1,
-                        score2=10,
-                    )
-                ],
-            )
-        ],
-    )
+    return tournament.gen_tournament()
 
 @router.put("/tournaments/{id}/matches/{match_id}")
-def put_tournament_info(id, match_id, score: Score):
-    print(score)
-    return {
-        "is_finished" : True
-    }
+def put_tournament_info(id: int, match_id: int, score: Score):
+    return tournament.put_tournament()
 
